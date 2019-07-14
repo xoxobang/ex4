c466119a3f9a98d3db54bd6f2c1a9152b9378bee
xoxobang <xoxo11112009@hotmail.com>
2019-07-14 11:52:06 +0700

    before change

---
+++ config/database.js
@@ -0,0 +1,11 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'todo'
+});
+
+connection.connect();
+
+module.exports = connection
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/play/:num1/:num2', function(req, res, next) {
+  //console.log(req.params)
+  var num1 = req.params.num1;
+ var num2 = req.params.num2;
+ var sum = parseInt(num1) + parseInt(num2);
+ res.send("ผลรวมของตัวเลข 2 จำนวนคือ " + sum);
+  res.render('play');
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BY KING BIG' });
+  res.render('index');
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'BY KING BIG' });
+router.get('/create', function(req, res, next) {
+  res.render('create');
 });
 
-router.get('/about/Tell', function(req, res, next) {
-  res.render('Tell', { title: 'BY KING BIG' });
+router.get('/login', function(req, res, next) {
+  res.render('login');
 });
 
 module.exports = router;
